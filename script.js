0025709eab53e8c82f35163778623cec07d1140a
Darren Kim <dkim555@yahoo.com>
2023-01-27 12:43:47 -0800

    create computer choices

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-    let playerScore = 0;
-    let computerScore = 0;
-    const rockChoice = document.getElementById('rock'); 
-    const paperChoice = document.getElementById('paper'); 
-    const scissorChoice = document.getElementById('scissor'); 
-    const playerChoices = [rockChoice, paperChoice, scissorChoice];
-    const computerChoice = ['rock', 'paper', 'scissor']
+let playerScore = 0;
+let computerScore = 0;
+
+const rockChoice = document.getElementById('rock'); 
+const paperChoice = document.getElementById('paper'); 
+const scissorChoice = document.getElementById('scissor'); 
+const playerChoices = [rockChoice, paperChoice, scissorChoice];
+let computerChoice = (Math.floor(Math.random() * 3 + 1));
+const computerChoices = ['rock', 'paper', 'scissor']
+
+
+function playGame() {
+    if(computerChoice === 1) {
+        computerChoice = 'rock'
+    };
+    if(computerChoice === 2) {
+        computerChoice = 'paper'
+    };
+    if(computerChoice === 3) {
+        computerChoice = 'scissor'
+    };
+    return computerChoice
+}
