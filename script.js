d4ef609c6b3707f3783bf0615b4b8c3c762fc2d2
Darren Kim <dkim555@yahoo.com>
2023-01-30 10:36:14 -0800

    write function for player choice

--- script.js
+++ script.js
@@ -1,60 +1,73 @@
-const playerScore = document.getElementById('playerScore');
-playerScore = 0;
-const computerScore = document.getElementById('computerScore');
-computerScore = 0;
+// const playerScore = document.getElementById('playerScore');
+// playerScore = 0;
+// const computerScore = document.getElementById('computerScore');
+// computerScore = 0;
 const winnerText = document.getElementById('winnerText'); 
-const playerChoices = document.getElementsByTagName('button');
+const computerChoiceText = document.getElementById('computer-Choice');
+const playerChoiceText = document.getElementById('player-Choice');
+const playerChoices = document.querySelectorAll('button');
+let playerChoice;
 
+// playerChoices.forEach(choice => choice.addEventListener('click', (e) => {
+//     playerChoice = e.target.id;
+//     playerChoiceText.innerHTML = playerChoice;
+// }))
 
-
-function playGame() {
-    let computerChoice = (Math.floor(Math.random() * 3 + 1));
-
-    if(computerChoice === 1) {
-        computerChoice = 'ROCK'
-    };
-    if(computerChoice === 2) {
-        computerChoice = 'PAPER'
-    };
-    if(computerChoice === 3) {
-        computerChoice = 'SCISSORS'
-    };
-    return computerChoice
-}
-
-const checkWinner = function(playerChoice,computerChoice){
-    if(playerChoice === computerChoice){
-        winnerText.innerHTML = 'TIE.';
+playerChoices.forEach(choice => {
+    choice.onclick = function() {userChoice()};
+    function userChoice() {
+        playerChoice = choice.innerText;
+        playerChoiceText.innerHTML = playerChoice;
     }
-    else if(playerChoice === 'ROCK'){
-        if(computerChoice === 'SCISSORS'){
-            winnerText.innerHTML = 'PLAYER WINS. ROCK BEATS SCISSORS.';
-            playerScore += 1;
-        }
-        else if(computerChoice === 'PAPER'){
-            winnerText.innerHTML= 'COMPUTER WINS. PAPER BEATS ROCK';
-            computerScore += 1;
-        }
-    }
-    else if(playerChoice === 'PAPER'){
-        if(computerChoice === 'SCISSORS'){
-            winnerText.innerHTML = 'COMPUTER WINS. SCISSORS BEATS PAPER.';
-            computerScore += 1;
-        }
-        else if(computerChoice === "ROCK"){
-            winnerText.innerHTML = 'PLAYER WINS. PAPER BEATS ROCK.';
-            playerScore += 1;
-        }
-    }
-    else if(playerChoice === "SCISSORS"){
-        if(computerChoice === "PAPER"){
-            winnerText.innerHTML = 'PLAYER WINS. SCISSORS BEATS PAPER';
-            playerScore += 1;
-        }
-        else if(computerChoice === "ROCK"){
-            winnerText.innerHTML = 'COMPUTER WINS ROCK BEATS SCISSORS';
-            computerScore += 1;
-        }
-    }
+});
+
+// function computerChoice() {
+//     let ranNum = (Math.floor(Math.random() * 3 + 1));
+
+//     if(ranNum === 1) {
+//         ranNum = 'ROCK'
+//     };
+//     if(ranNum === 2) {
+//         ranNum = 'PAPER'
+//     };
+//     if(ranNum === 3) {
+//         ranNum = 'SCISSORS'
+//     };
+// }
+
+// function checkWinner(player,computer){
+//     if(player === computer){
+//         winnerText.innerHTML = 'TIE.';
+//     }
+//     else if(player === 'ROCK'){
+//         if(computer === 'SCISSORS'){
+//             winnerText.innerHTML = 'PLAYER WINS. ROCK BEATS SCISSORS.';
+//             playerScore += 1;
+//         }
+//         else if(computer=== 'PAPER'){
+//             winnerText.innerHTML= 'CO MPUTER WINS. PAPER BEATS ROCK';
+//             computerScore += 1;
+//         }
+//     }
+//     else if(player === 'PAPER'){
+//         if(computer === 'SCISSORS'){
+//             winnerText.innerHTML = 'COMPUTER WINS. SCISSORS BEATS PAPER.';
+//             computerScore += 1;
+//         }
+//         else if(computer === "ROCK"){
+//             winnerText.innerHTML = 'PLAYER WINS. PAPER BEATS ROCK.';
+//             playerScore += 1;
+//         }
+//     }
+//     else if(player === "SCISSORS"){
+//         if(computer === "PAPER"){
+//             winnerText.innerHTML = 'PLAYER WINS. SCISSORS BEATS PAPER';
+//             playerScore += 1;
+//         }
+//         else if(computer === "ROCK"){
+//             winnerText.innerHTML = 'COMPUTER WINS ROCK BEATS SCISSORS';
+//             computerScore += 1;
+//         }
+//     }
     
-}
+// }
